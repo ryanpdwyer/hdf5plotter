97f379be5fa5978504be0e7b1834d2789f1466c4
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-10-21 14:48:01 -0400

    Added a tested map function

--- hdf5plotter/_plotters.py
+++ hdf5plotter/_plotters.py
@@ -4,7 +4,7 @@ from __future__ import division, print_function
 import h5py
 import matplotlib as mpl
 
-from hdf5plotter import silent_del, u
+from hdf5plotter import silent_del, u, update_attrs
 from hdf5plotter._util import (replace_unit_label, replace_latex_label,
                                get_unit_attr)
 
@@ -27,6 +27,10 @@ class PlotFromManyFiles(object):
             group.file.close()
 
     def rescale(self, old_dset, new_dset, new_unit):
+        """Rescale data in old_dset to the units new_unit, and put the result
+        in a dataset named new_dset."""
+        if new_dset == old_dset:
+            raise ValueError("'new_dset' and 'old_dset' must be different.")
         for group in self.groups:
             # Make sure the new dataset doesn't exist, so we can define it
             silent_del(group, new_dset)
@@ -34,12 +38,24 @@ class PlotFromManyFiles(object):
             old_unit = u(group[old_dset].attrs['unit'])
             scaled_dset = (group[old_dset][:] * old_unit).to(new_unit).magnitude
             group[new_dset][:] = scaled_dset
+
             new_attrs = group[new_dset].attrs
+            # Update the unit in the new_attrs
             new_attrs['unit'] = get_unit_attr(new_unit)
             new_attrs['label'] = replace_unit_label(
                 new_attrs['label'], new_unit)
             new_attrs['label_latex'] = replace_latex_label(
                 new_attrs['label_latex'], new_unit)
+
+    def map(self, function, new_dset, new_attrs):
+        """Transforms data using function, and puts the result in new_dset, and
+        attachs the attributes new_attrs."""
+        for group in self.groups:
+            # Make sure the new dataset doesn't exist, so we can define it
+            silent_del(group, new_dset)
+            group[new_dset] = function(group)
+            update_attrs(group[new_dset].attrs, new_attrs)
+
 
 
     def plot(self, x='x', y='y', scale='linear', shape='-', xlim=None, ylim=None,
--- hdf5plotter/tests/test_hdf5plotter.py
+++ hdf5plotter/tests/test_hdf5plotter.py
@@ -82,7 +82,7 @@ class TestPlotFromManyFiles_rescale(unittest.TestCase):
                               (u'help', u'Time example')))
         update_attrs(self.f['x'].attrs, x_attrs)
 
-        y_attrs = OrderedDict(((u'name', "Displacement"),
+        y_attrs = OrderedDict(((u'name', "Displacement Squared"),
                               (u'unit', u'nm^2'),
                               (u'label', u"z [nm\xb2]"),
                               (u'label_latex', u'$z \\: [\\mathrm{nm}^2]$'),
@@ -93,10 +93,10 @@ class TestPlotFromManyFiles_rescale(unittest.TestCase):
         self.plotter = PlotFromManyFiles()
         self.plotter.add(self.filename)
 
-    def test_rescale(self):
+    def test_rescale_normal(self):
         x_sec = self.f['x'][:] / 1000
         self.plotter.rescale('x', 'x_s', 's')
-        assert_array_max_ulp(self.plotter.groups[0]['x_s'][:], x_sec)
+        assert_array_max_ulp(self.plotter.groups[0]['x_s'][:], x_sec, 5)
 
         x_s_attrs = self.f['x_s'].attrs
 
@@ -110,13 +110,13 @@ class TestPlotFromManyFiles_rescale(unittest.TestCase):
         for key, val in exp_x_s_attrs.items():
             eq_(val, x_s_attrs[key])
 
-    def test_rescale_um(self):
+    def test_rescale_squared_micro_prefix(self):
         y_um = self.f['y'][:] / 1000**2
         self.plotter.rescale('y', 'y_um', u.um**2)
         assert_array_max_ulp(self.plotter.groups[0]['y_um'][:], y_um, 10)
         y_um_attrs = self.f['y_um'].attrs
         exp_y_um_attrs = OrderedDict((
-            (u'name', "Displacement"),
+            (u'name', "Displacement Squared"),
             (u'unit', u'um^2'),
             (u'label', u"z [\xb5m\xb2]"),
             (u'label_latex', u'$z \\: [\\mu\\mathrm{m}^{2}]$'),
@@ -125,6 +125,61 @@ class TestPlotFromManyFiles_rescale(unittest.TestCase):
 
         for key, val in exp_y_um_attrs.items():
             eq_(val, y_um_attrs[key])
+
+    def tearDown(self):
+        self.f.close()
+        silentremove(self.filename)
+
+
+class TestPlotFromManyFiles_map(unittest.TestCase):
+    filename = '.TestPlotFromManyFiles_map.h5'
+
+    def setUp(self):
+        silentremove(self.filename)
+        self.f = h5py.File(self.filename)
+        self.f['x'] = np.arange(0, 10, 0.1, dtype=np.float64)
+        self.f['y'] = 100*np.sin(self.f['x'][:])
+        x_attrs = OrderedDict(((u'name', "Time"),
+                              (u'unit', u'ms'),
+                              (u'label', u"Time [ms]"),
+                              (u'label_latex', u'Time $t \\: [\\mathrm{ms}]$'),
+                              (u'help', u'Time example')))
+        update_attrs(self.f['x'].attrs, x_attrs)
+
+        y_attrs = OrderedDict(((u'name', "Displacement Squared"),
+                              (u'unit', u'nm^2'),
+                              (u'label', u"z [nm\xb2]"),
+                              (u'label_latex', u'$z \\: [\\mathrm{nm}^2]$'),
+                              (u'help', u'Displacement example'),
+                              (u'n_avg', 1.0)))
+        update_attrs(self.f['y'].attrs, y_attrs)
+
+        self.plotter = PlotFromManyFiles()
+        self.plotter.add(self.filename)
+
+    def test_square_root_y(self):
+
+        new_attrs = OrderedDict((
+            (u'name', "Displacement"),
+            (u'unit', u'nm'),
+            (u'label', u"z [nm]"),
+            (u'label_latex', u'$z \\: [\\mathrm{nm}]$'),
+            (u'help', u'Displacement example'),
+            (u'n_avg', 1.0)))
+
+        exp_y_sqrt = 10*np.sqrt(np.abs(np.sin(self.f['x'][:])))
+
+        def transform(group):
+            return np.abs(group['y'][:])**0.5
+
+        self.plotter.map(transform, 'y_sqrt', new_attrs)
+
+        y_sqrt = self.plotter.groups[0]['y_sqrt']
+
+        assert_array_max_ulp(exp_y_sqrt, y_sqrt[:], 5)
+
+        for key, val in new_attrs.items():
+            eq_(val, y_sqrt.attrs[key])
 
     def tearDown(self):
         self.f.close()
